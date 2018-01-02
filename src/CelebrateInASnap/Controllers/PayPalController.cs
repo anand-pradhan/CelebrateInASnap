fe9a8facb49b350ebf041d7040f5c6198522a53b
pay1986 <pay1986@live.com>
2018-01-01 23:14:58 -0800

    Add project files.

---
+++ src/CelebrateInASnap/Controllers/PayPalController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CelebrateInASnap.Interfaces;
+using CelebrateInASnap.Services;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CelebrateInASnap.Controllers
+{
+    public class PayPalController : Controller
+    {
+        private readonly IPayPalPaymentService _payPalPaymentService;
+        public PayPalController(IPayPalPaymentService payPalPaymentService)
+        {
+            _payPalPaymentService = payPalPaymentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CreatePayment()
+        {
+            var payment = _payPalPaymentService.CreatePayment(GetBaseUrl(), "sale");
+
+            return Redirect(payment.GetApprovalUrl());
+        }
+
+        public IActionResult PaymentCancelled()
+        {
+            // TODO: Handle cancelled payment
+            return RedirectToAction("Error");
+        }
+
+        public IActionResult PaymentSuccessful(string paymentId, string token, string PayerID)
+        {
+            // Execute Payment
+            var payment = _payPalPaymentService.ExecutePayment(paymentId, PayerID);
+
+            return View();
+        }
+        private string GetBaseUrl()
+        {
+            return Request.Scheme + "://" + Request.Host;
+        }
+    }
+}
---
+++ src/CelebrateInASnap/Interfaces/IPayPalPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CelebrateInASnap.Interfaces
+{
+    public interface IPayPalPaymentService
+    {
+        Payment CreatePayment(string baseUrl, string intent);
+        Payment ExecutePayment(string paymentId, string payerId);
+    }
+}
